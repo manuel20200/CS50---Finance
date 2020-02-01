a5e869d2bcdb58faf69c811544e880e9eabc555c
manuel20200 <manuel20200@users.noreply.github.com>
2020-02-01 17:49:34 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -10,31 +10,31 @@ int main(void)
     //////////// Require and check Height between 1 and 9/////////
     do
     {
-    Height = get_int("Height: ");    
+        Height = get_int("Height: ");    
     } 
     while (Height <= 0 || Height > 8);
     //////////////////////////////////////////////////
         
-     for (int i = 0; i < Height; i++)  ///// loop for the levels of pyramid
-     {
-         for (int j = 0; j < (Height-i-1); j++)   //// loop for spaces of the left side
-         {
-          printf (" "); 
-         } 
+    for (int i = 0; i < Height; i++)  //loop for the levels of pyramid
+    {
+        for (int j = 0; j < (Height - i - 1); j++)   //loop for spaces of the left side
+        {
+          printf(" "); 
+        } 
                   
-         for (int k = 0; k <= i; k++)   /// loop for left side of the pyramid (blocks)
-         {
-          printf ("#"); 
-         } 
+        for (int k = 0; k <= i; k++)   //loop for left side of the pyramid (blocks)
+        {
+          printf("#"); 
+        } 
          
-          printf ("  ");  ////// central spaces
+        printf("  ");  // central spaces
          
-           for (int l = 0; l <= i; l++)   ///// loop for right side of the pyramid (blocks)
-         {
-          printf ("#");   
-         }
+        for (int l = 0; l <= i; l++)   //loop for right side of the pyramid (blocks)
+        {
+          printf("#");   
+        }
          
-          printf ("\n");  /// final jump   
-      }    
+        printf("\n");  //final jump   
+    }    
     
 }
