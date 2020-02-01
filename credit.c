0bfe419adad9f12a290b27a8017f1c6f522008bc
manuel20200 <manuel20200@users.noreply.github.com>
2020-02-01 20:22:22 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- credit.c
+++ credit.c
@@ -11,48 +11,52 @@ int main (void)
     int size = 0;   //number of digits in the card number
     int c1,c2 = 0;  //c1 first digit   c2 second digit   from left to right
     
-    while (number > 0)
+    while (number > 0)  //from number we'll get every digit, retiring every digit from number until we get only one digit
     {
-        int digit = number % 10;
-        number = number / 10;
+        int digit = number % 10;    //get every digit
+        number = number / 10;   //when we get digit we retire the digit from number
         size++;
 
-        if (number < 10 && number > 0)
+        if (number < 10 && number > 0)  //get the second digit  left to right
         { 
             c2 = digit;
         }
-        if (number == 0)
+        if (number == 0)    //get the first digit  left to right
         {
              c1 = digit;
         }
     
-        if ( i == 0 )
+        if ( i == 0 ) //add digit that musnt be multiply by 2
         {
             i = 1;
             checksum = checksum + digit;
         }
-        else
+        else    //add digit that must be multiply by 2
         {
             i = 0;
-            if (2*digit >= 10)
+            if (2*digit >= 10)  //if result of multiplication by 2 is two digits, add the two digits to checksum
             {
-                int digit1 = (2*digit)%10;
-                checksum = checksum+digit1+1;
+                int digit1 = (2 * digit) % 10;
+                checksum = checksum + digit1 + 1;
             }
-            else 
+            else    //if result of multiplication by 2 is one digit, add to checksum
             {
-                checksum = (2*digit)+checksum;
+                checksum = (2 * digit) + checksum; 
             }
         }
             
         //printf("Digit : %i %li %i %i %i %i\n",digit,number,c1,c2,checksum, size);  //only for testing 
     }
 
-    if (size >= 13 && size <= 16 && c1 == 4 && (checksum % 10 == 0))
+    //VISA          SIZE: 13 to 16      FIRST NUMBERS: 4
+    //AMEX          SIZE: 15            FIRST NUMBERS: 34 or 37
+    //MASTERCARD    SIZE: 16            FIRST NUMBERS: 51 to 55
+
+    if (size >= 13 && size <= 16 && c1 == 4 && (checksum % 10 == 0))    //check VISA
     {
         printf("VISA\n");
     }
-    else if (size == 15 && c1 == 3 && (c2 == 4 || c2 == 7) && (checksum % 10 == 0))
+    else if (size == 15 && c1 == 3 && (c2 == 4 || c2 == 7) && (checksum % 10 == 0)) //check AMEX
     {
         printf("AMEX\n");
     }
