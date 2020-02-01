dea920b04172e3100106847b02287d11f76d4634
manuel20200 <manuel20200@users.noreply.github.com>
2020-02-01 21:21:22 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -3,40 +3,40 @@
 #include <math.h>
 
 float change = 0;
-int changeI, quarters, dimes, nickels, pennies = 0;
+int changeI, quarters, dimes, nickels, pennies = 0; //coins number for every value
 
-int main (void)
+int main(void)
 {
-while (change <= 0)
-{
-    change = get_float ("Change Owe: " );
-}
+    while (change <= 0) //get permited value
+    {
+        change = get_float("Change Owe: ");
+    }
 
-changeI = round (change*100);
+    changeI = round(change * 100);    //convert Float to Int
 
-if (changeI >= 25)
-{  
-    quarters = changeI / 25;
-    changeI = changeI - (quarters * 25);
-    //printf ("%i %i \n",quarters,changeI);
-}
-if (changeI >= 10)
-{
-    dimes = changeI / 10;
-    changeI = changeI - (dimes * 10);
-    //printf ("%i %i \n",dimes,changeI);
-}
-if (changeI >= 5)
-{
-    nickels = changeI / 5;
-    changeI = changeI - (nickels * 5);
-    //printf ("%i %i \n",nickels,changeI);
-}
-if (changeI < 5)
-{
-    pennies = changeI;
-    //printf ("%i %i \n",pennies,changeI);
-}
-printf("Coins= %i \n", (quarters + dimes + nickels + pennies)); 
+    if (changeI >= 25)  //get number of quarters
+    {  
+        quarters = changeI / 25;
+        changeI = changeI - (quarters * 25);
+        //printf ("%i %i \n",quarters,changeI);
+    }
+    if (changeI >= 10) //get number of dimes
+    {
+        dimes = changeI / 10;
+        changeI = changeI - (dimes * 10);
+        //printf ("%i %i \n",dimes,changeI);
+    }
+    if (changeI >= 5)   //get number of nickels
+    {
+        nickels = changeI / 5;
+        changeI = changeI - (nickels * 5);
+        //printf ("%i %i \n",nickels,changeI);
+    }
+    if (changeI < 5) //get number of pennies
+    {
+        pennies = changeI;
+        //printf ("%i %i \n",pennies,changeI);
+    }
+    printf("Coins= %i \n", (quarters + dimes + nickels + pennies)); 
     
 }
