c8b154e1d93c97a8386a3fc2357878a931dc7e73
manuel20200 <manuel20200@users.noreply.github.com>
2020-02-16 03:01:10 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -13,7 +13,8 @@ int main(int argc, string argv[])
         string ciphertext = " ";
         code[0] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         code[1] = "abcdefghijklmnopqrstuvwxyz";
-        code[2] = "zaqwsxcderfvbgtyhnmjuiklop";
+        code[2] = "                          ";
+        char caracter[26];
         int aux[5] = {0, 0, 0, 0, 0};
         int validletter = 0;
         int check = 0;
@@ -22,17 +23,37 @@ int main(int argc, string argv[])
         validletter = 0;
         for (int i = 0, n = strlen(argv[1]); i <= n; i++)
         {
+
             if ((argv[1][i] >= 'a' && argv[1][i] <= 'z') || (argv[1][i] >= 'A' && argv[1][i] <= 'Z'))
             {
                 validletter++;
             }
-            check = check + toupper(argv[1][i]);
+            for (int j = 0; j < 26; j++)
+            {
+                if ((argv[1][i] == code[0][j]) || (argv[1][i] == code[1][j]))
+                {
+                    caracter[j] = '1';
+                    //printf("%i %c %c\n", j, caracter[0], caracter[5]);
+                }
+            }
         }
         size = strlen(argv[1]);
-        //printf("%s %s %i %i %i \n", plaintext, argv[1], validletter, check, aux[2]);
+        //printf("%s %s %i %i %s \n", plaintext, argv[1], validletter, check, code[2]);
+        for (int i = 0; i < 26; i++)
+        {
+
+            if (caracter[i] == '1')
+            {
+                check++;
+            }
+            else
+            {
+                check--;
+            }
+        }
 
 
-        if (size != 26 || validletter != 26 || check != 2015)
+        if (size != 26 || validletter != 26 || check != 26)
         {
             printf("Please digit a valid key.\n");
             return 1;
