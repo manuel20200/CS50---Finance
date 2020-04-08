040fe6b2546678475e8b3919473dc5667d0cdb42
manuel20200 <manuel20200@users.noreply.github.com>
2020-04-08 22:50:56 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -10,10 +10,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int avg = (image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen) / 3;
-            image[i][j].rgbtRed = avg;
-            image[i][j].rgbtBlue = avg;
-            image[i][j].rgbtGreen = avg;
+            float avg = (image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen) / 3;
+            image[i][j].rgbtRed = round(avg);
+            image[i][j].rgbtBlue = round(avg);
+            image[i][j].rgbtGreen = round(avg);
         }
     }
     return;
