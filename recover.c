6de3da7947cf900078f5c2ccf195b9d791cf3a25
manuel20200 <manuel20200@users.noreply.github.com>
2020-04-13 22:36:12 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -7,6 +7,7 @@ int count = 0;
 int retorno = 512;
 char filename[8];
 unsigned char bytes[512];
+int i = 0;
 
 FILE *img;
 
@@ -33,42 +34,40 @@ int main(int argc, char *argv[])
     {
         int dir = ftell(file);
         dir = ftell(file);
-        //printf("%i -------------------------------------------- \n", retorno);
 
-        for (int i = 0; i < 1; i++)
+
+        //check the first 4 bytes for 0xff // 0xd8 // 0xff // 0xe, if equals posible jpg
+        if (bytes[i] == 0xff && bytes[i + 1] == 0xd8 && bytes[i + 2] == 0xff && (bytes[i + 3] & 0xf0) == 0xe0)
         {
-            //check the first 4 bytes for 0xff // 0xd8 // 0xff // 0xe, if equals posible jpg
-            if (bytes[i] == 0xff && bytes[i + 1] == 0xd8 && bytes[i + 2] == 0xff && (bytes[i + 3] & 0xf0) == 0xe0)
+            //check if there is a jpeg file actually open
+            if (open == 1)
             {
-                //check if there is a jpeg file actually open
-                if (open == 1)
-                {
-                    fclose(img);
-                    open = 0;
-                }
-
-                //every start of jpg file increase count by 1
-                count++;
-                int result = sprintf(filename, "%03i.jpg", (count - 1));
-
-                //FILE *img = fopen(filename, "w");
-                img = fopen(filename, "w");
-                if (img == NULL)
-                {
-                    fprintf(stderr, "Could not open img %s.\n", filename);
-                    return 4;
-                }
-                //open indicate if there is a jpg file open
-                open = 1;
-                fwrite(bytes, sizeof(char), 512, img);
+                fclose(img);
+                open = 0;
             }
-            else if (open == 1)
+
+            //every start of jpg file increase count by 1
+            count++;
+            int result = sprintf(filename, "%03i.jpg", (count - 1));
+
+            //FILE *img = fopen(filename, "w");
+            img = fopen(filename, "w");
+            if (img == NULL)
             {
-
-                fwrite(bytes, sizeof(char), 512, img);
-
+                fprintf(stderr, "Could not open img %s.\n", filename);
+                return 4;
             }
+            //open indicate if there is a jpg file open
+            open = 1;
+            fwrite(bytes, sizeof(char), 512, img);
         }
+        else if (open == 1)
+        {
+
+            fwrite(bytes, sizeof(char), 512, img);
+
+        }
+
         //int retorno = fread(arr, sizeof(char), 512, file);
         retorno = fread(bytes, sizeof(char), 512, file);
     }
